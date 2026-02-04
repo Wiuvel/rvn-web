858f7d7fc0864e52167df00004072035295fd65e
Wiuvel <wiuvel@outlook.com>
2026-02-04 10:28:42 +0300

    re: fix internal error prod, disable cache components

--- app/ui/panel/admin/page.tsx
+++ app/ui/panel/admin/page.tsx
@@ -1,12 +1,7 @@
-import { cacheLife, cacheTag } from 'next/cache';
 import { getTeamCount } from '@/lib/data/team';
 import AdminPanelContent from '@/components/admin/AdminPanelContent';
 
 export default async function AdminPanelPage() {
-  'use cache';
-  cacheLife('minutes');
-  cacheTag('admin-team');
-
   const teamStats = await getTeamCount();
 
   return <AdminPanelContent teamCount={teamStats.count} />;
